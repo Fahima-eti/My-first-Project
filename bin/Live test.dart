15d1a5b0a0b5d94e18474e72f983ffff95e0f871
Fahima Akter <fahimaaktereti31@gmail.com>
2025-01-01 23:21:06 +0600

    updated this file

---
+++ bin/Live test.dart
@@ -0,0 +1,7 @@
+void calculateArea(double area1,double area2,double area3) {
+  double triangleArea = (area1 * area2 * area3);
+  print("Triangle Area:$triangleArea");
+}
+void main(){
+  calculateArea(0.5, 0.4, 0.6);
+}
